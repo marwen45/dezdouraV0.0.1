512bceb8ec984c6b6774a71bdce93a5b2842fdda
marwen <marwen.hammami@myu.universitecentrale.tn>
2021-06-06 11:46:29 +0100

    forgot password marwen hammami

--- forgotpw.php
+++ forgotpw.php
@@ -1,3 +1,56 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+
+$msg="";
+include ('class/connectdb.php');
+
+if (isset($_POST['submit'])){
+        $email=$_POST['email'];
+        if ($email=='')
+                $msg="Please fill up your inputs!";
+        else{
+                $mail = new PHPMailer();
+
+try {
+
+                        //Server settings
+                        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+                        $mail->isSMTP();                                            //Send using SMTP
+                        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+                        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                        $mail->Username   = 'noreplyb0t.tn@gmail.com';                     //SMTP username
+                        $mail->Password   = 'marouene125MM@';                               //SMTP password
+                        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+                        $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+                        //Recipients
+                        $mail->setFrom('noreplyb0t.tn@gmail.com', 'DezDoura Support');
+                        $mail->addAddress($email);     //Add a recipient
+                        //Content
+                        $mail->isHTML(true);                                  //Set email format to HTML
+                        $mail->Subject = 'reset password';
+                        $mail->Body    = "
+                        Please click on the link below:<br><br>
+                        <a href='http://localhost:8000/newpw.php?email=$email'>Click Here</a>
+                    ";     
+
+                    $mail->send();
+                    $msg = "You have been registered! Please verify your email!";
+                } catch (Exception $e) {
+                        $msg = "Something wrong happened! Please try again!: {$mail->ErrorInfo}";
+                
+                }
+                
+                }
+        }
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,7 +71,7 @@
                                                         <p></p>
 															<input name="email" type="text" placeholder="email" class="input"><br></br>														
 													</div>
-													<div name="login" class="btn"><a href="" onclick='document.getElementById("test").submit()'>send</a></div>		
+													<div name ="send" class="btn"><a href="#"><input type="submit" name="submit" class="button_active" value="send"></a></div>	
 											</div>
 										</form>              
 			</div>
--- newpw.php
+++ newpw.php
@@ -1,3 +1,37 @@
+<?php
+
+$msg="";
+include ('class/connectdb.php');
+function redirect() {
+	header('Location: login.php');
+	exit();
+}
+if (isset($_POST['submit'])||!isset($_GET['email'])){
+	redirect();
+	}
+	else {
+		$email = $_GET['email'];
+		$password=$_POST['password'];
+		$cPassword=$_POST['cPassword']; 
+
+
+        if ($password!=$cPassword)
+			$hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+		else {
+			$sql = "SELECT id FROM users WHERE email='$email'";
+			$result = $conn->query($sql);
+			
+		if ($result->num_rows > 0) {
+			$sqlu=("UPDATE users SET password=$cPassword WHERE email='$email'");
+            $resultu = $conn->query($sqlu);
+			$msg='set new password!';
+		} else
+			redirect();
+	}
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,14 +46,18 @@
 			<div class="logo">
 				<img src="./assets/img/dezdoura.png" alt="" style="width: 230px;">
 			</div>                      
+
+			<div>
+                        <?php echo $msg . "<br><br>" ?>
+                        </div>
 			 							<form id= "test" action="" method="post">    
 											<div class="form">
 													<div class="input_field">
                                                         <p></p>
-															<input name="pw" type="text" placeholder="new password" class="input"><br></br>
-                                                            <input name="pw" type="text" placeholder="retype new password" class="input"><br></br>													
+														<input name="password" type="password" placeholder="Password" class="input"><br></br>
+                                                        <input name="cPassword" type="password" placeholder="confirm Password" class="input"><br></br>													
 													</div>
-													<div name="login" class="btn"><a href="" onclick='document.getElementById("test").submit()'>save</a></div>		
+													<div name ="send" class="btn"><a href="#"><input type="submit" name="submit" class="button_active" value="save"></a></div>	
 											</div>
 										</form>              
 			</div>
